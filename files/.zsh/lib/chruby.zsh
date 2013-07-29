0c7f681887352482920e56b64d663f06b1f17086
Wim Looman <ghostunderscore@gmail.com>
2013-07-29 13:53:17 +1200

    Add chruby init script

---
+++ files/.zsh/lib/chruby.zsh
@@ -0,0 +1,11 @@
+function () {
+  chruby_config_files=(
+    /usr/local/share/chruby/chruby.sh
+    /usr/local/share/chruby/auto.sh
+  )
+
+  for config_file in $chruby_config_files
+    [[ -s $config_file ]] && source $config_file
+
+  chruby 2.0
+}
