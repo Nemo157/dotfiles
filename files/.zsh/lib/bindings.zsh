262f80ebc2a5dc2d4cee689d11e93904c35d8eb2
Wim Looman <ghostunderscore@gmail.com>
2013-07-29 13:51:34 +1200

    Add bindings back in if the map file exists

--- files/.zsh/lib/bindings.zsh
+++ files/.zsh/lib/bindings.zsh
@@ -1,21 +1,26 @@
 # Load the prepared key mappings
 # If you need to generate some of these simply run:
 #     autoload zkbd; zkbd
-map_file="$ZDOTDIR/.zkbd/$TERM-${DISPLAY:-$VENDOR-$OSTYPE}"
-[[ -f $map_file ]] && source $map_file
+function () {
+  setup_bindings () {
 
-# Setup the bindings
-#typeset -A bindings
-#bindings[Left]=backward-char
-#bindings[Right]=forward-char
-#bindings[Home]=beginning-of-line
-#bindings[End]=end-of-line
-#bindings[Delete]=delete-char
-#bindings[CtrlSpace]=complete-word
+    # Setup the bindings
+    typeset -A bindings
+    bindings[Left]=vi-backward-char
+    bindings[Right]=vi-forward-char
+    bindings[Home]=beginning-of-line
+    bindings[End]=end-of-line
+    bindings[Delete]=vi-delete-char
+    bindings[CtrlSpace]=complete-word
 
-# Change to vi mode
-bindkey -v
+    # Change to vi mode
+    bindkey -v
 
-# Apply the bindings
-#for k in ${(k)bindings}
-#  [[ -n ${key[$k]} ]] && bindkey "${key[$k]}" "${bindings[$k]}" || bindkey "$k" "${bindings[$k]}"
+    # Apply the bindings
+    for k in ${(k)bindings}
+      [[ -n ${key[$k]} ]] && bindkey "${key[$k]}" "${bindings[$k]}" || bindkey "$k" "${bindings[$k]}"
+  }
+
+  local map_file="$ZDOTDIR/.zkbd/$TERM-${${DISPLAY:t}:-$VENDOR-$OSTYPE}"
+  [[ -f $map_file ]] && source $map_file && setup_bindings
+}
