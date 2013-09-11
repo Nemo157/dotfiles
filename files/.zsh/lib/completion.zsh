a769c388170eaa2592b7b487f695950edcd79fae
Wim Looman <ghostunderscore@gmail.com>
2013-09-11 17:48:41 +1200

    Add sudo specific path to get /sbin completion

--- files/.zsh/lib/completion.zsh
+++ files/.zsh/lib/completion.zsh
@@ -7,6 +7,8 @@ zstyle ':completion:*:*:*:*:*' menu select
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
 zstyle ':completion:*:*:*:*:processes' command "ps -u `whoami` -o pid,user,comm -w -w"
 
+zstyle ':completion:*:sudo:*' command-path /usr/local/sbin /usr/local/bin /usr/sbin /usr/bin /sbin /bin
+
 # disable named-directories autocompletion
 zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
 cdpath=(. ~ ~/sources)
