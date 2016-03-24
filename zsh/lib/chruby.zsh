65402eb82414cd0c5ad2c6ba50d88589f1b9ba95
Wim Looman <wim.looman@kiwi.ki>
2016-03-24 10:18:39 +0100

    Add in llvm symbolizer path so ubsan can give nice stacktraces

--- zsh/lib/chruby.zsh
+++ zsh/lib/chruby.zsh
@@ -1,7 +1,7 @@
 function () {
   chruby_config_files=(
-    /usr/local/share/chruby/chruby.sh
-    /usr/local/share/chruby/auto.sh
+    "/usr/local/share/chruby/chruby.sh"
+    "/usr/local/share/chruby/auto.sh"
   )
 
   for config_file in $chruby_config_files
