978f421e8eedba630c7fd293f54f30f0f105eb89
Wim Looman <wim.looman@kiwi.ki>
2016-09-06 15:11:07 +0200

    Use scripts for extra status line parameters

---
+++ tmux/bin/batt_status.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+[ -f /sys/class/power_supply/BAT0/status ] || exit 1
+[ -f /sys/class/power_supply/BAT0/energy_now ] || exit 1
+[ -f /sys/class/power_supply/BAT0/energy_full ] || exit 1
+
+case $(cat /sys/class/power_supply/BAT0/status) in
+  Discharging)
+    symbol='🔋'
+    ;;
+esac
+
+percent=$(python -c "print('%2d' % ($(cat /sys/class/power_supply/BAT0/energy_now).0 / $(cat /sys/class/power_supply/BAT0/energy_full).0 * 100))")
+
+echo "$symbol $percent%"
