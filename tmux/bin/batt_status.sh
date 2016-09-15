1f6d1dcf18d5868da221ccf2a42034106f0f3421
Wim Looman <wim.looman@kiwi.ki>
2016-09-15 10:17:45 +0200

    Support charging symbol on linux

--- tmux/bin/batt_status.sh
+++ tmux/bin/batt_status.sh
@@ -8,6 +8,9 @@ case $(cat /sys/class/power_supply/BAT0/status) in
   Discharging)
     symbol='🔋'
     ;;
+  Charging)
+    symbol='🔌'
+    ;;
 esac
 
 percent=$(python -c "print('%2d' % ($(cat /sys/class/power_supply/BAT0/energy_now).0 / $(cat /sys/class/power_supply/BAT0/energy_full).0 * 100))")
