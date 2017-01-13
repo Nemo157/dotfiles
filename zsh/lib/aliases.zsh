0697b1f71b4556f700918558eafd4d5b1e21fdc3
Wim Looman <wim.looman@kiwi.ki>
2017-01-13 17:15:51 +0100

    Add alias for open->xdg-open, remove old aliases

--- zsh/lib/aliases.zsh
+++ zsh/lib/aliases.zsh
@@ -3,14 +3,15 @@ alias cd..='cd ..'
 alias ls='ls -h'
 alias ll='ls -l'
 alias lal='ls -al'
-if [[ -x $(which gdu) ]]; then
-  alias sizes='gdu -hs ./* | gsort -hr | head'
-else
-  alias sizes='du -hs ./* | sort -hr | head'
-fi
 alias man=run-help
 
-if [[ -x $(which sudo) ]] alias fu='sudo $( fc -ln -1 )'
+if [[ -x $(which sudo) ]] {
+  alias fu='sudo $( fc -ln -1 )'
+}
 
-alias tmux_mutt='tmux new-window -t 9 env EDITOR=vim mutt'
+alias tmux_mutt='tmux new-window -t 9 mutt'
 alias tmux_irssi='tmux new-window -t 10 irssi'
+
+if (! (which open >&-) && (which xdg-open >&-)) {
+  alias open='xdg-open'
+}
