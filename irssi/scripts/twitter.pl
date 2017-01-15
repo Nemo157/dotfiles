d322bab6e806c4029e2b3078b0ba01ed7d329f39
Wim Looman <wim@nemo157.com>
2017-01-15 11:59:59 +0100

    Handle error when getting tweet

--- irssi/scripts/twitter.pl
+++ irssi/scripts/twitter.pl
@@ -20,32 +20,37 @@ $VERSION = "1";
 sub public {
   my ($server, $msg, $nick, $address, $target) = @_;
 
-  if ($msg =~ /.*https?:\/\/twitter\.com\/[^\/]+\/status\/([^\/]+)/) {
+  if ($msg =~ /.*https?:\/\/twitter\.com\/[^\/ ]+\/status\/([^\/ ]+)/) {
     my $id = $1;
 
-    my $config_file = Irssi::settings_get_str('twitter_config_file');
-    if (!$config_file) {
-      Irssi::print("twitter: twitter_config_file not set");
-      return;
+    eval {
+      my $config_file = Irssi::settings_get_str('twitter_config_file');
+      if (!$config_file) {
+        Irssi::print("twitter: twitter_config_file not set");
+        return;
+      }
+
+      my $config = LoadFile(expand_filename($config_file));
+      if (!$config) {
+        Irssi::print("twitter: twitter_config_file not readable");
+        return;
+      }
+
+      my $twitter = Net::Twitter->new(
+        traits   => [qw/API::RESTv1_1/],
+        consumer_key        => $config->{consumer_key},
+        consumer_secret     => $config->{consumer_secret},
+        access_token        => $config->{access_token},
+        access_token_secret => $config->{access_token_secret},
+      );
+
+      my $status = $twitter->show_status($id);
+      my $text = $status->{text};
+      $server->print($target, "twit: $text", MSGLEVEL_CLIENTNOTICE | MSGLEVEL_NO_ACT);
+    };
+    if ($@) {
+      Irssi::print("twitter error: $@");
     }
-
-    my $config = LoadFile(expand_filename($config_file));
-    if (!$config) {
-      Irssi::print("twitter: twitter_config_file not readable");
-      return;
-    }
-
-    my $twitter = Net::Twitter->new(
-      traits   => [qw/API::RESTv1_1/],
-      consumer_key        => $config->{consumer_key},
-      consumer_secret     => $config->{consumer_secret},
-      access_token        => $config->{access_token},
-      access_token_secret => $config->{access_token_secret},
-    );
-
-    my $status = $twitter->show_status($id);
-    my $text = $status->{text};
-    $server->print($target, "twit: $text", MSGLEVEL_CLIENTNOTICE | MSGLEVEL_NO_ACT);
   }
 }
 
