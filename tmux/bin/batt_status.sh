b6cfdd9c48814b811807ac2b3c090ca1b1ed6ad0
Wim Looman <wim.looman@kiwi.ki>
2017-04-26 10:56:58 +0200

    Calculate time remaining for charging on linux

--- tmux/bin/batt_status.sh
+++ tmux/bin/batt_status.sh
@@ -10,38 +10,47 @@ time='?:??'
 
 BAT0=/sys/class/power_supply/BAT0
 
-if [ -f $BAT0/status ]
+if [ -f $BAT0/status ] && [ -f $BAT0/capacity ] && [ -f $BAT0/power_now ] && [ -f $BAT0/energy_now ]
 then
   case $(cat $BAT0/status) in
-    Discharging) symbol=$battery ;;
-    Charging) symbol=$power ;;
+    Discharging)
+      symbol=$battery
+      if [ "$(cat $BAT0/power_now)" = 0 ]
+      then
+        time='0:00'
+      else
+        minutes=$(( $(cat $BAT0/energy_now) * 60 / $(cat $BAT0/power_now) ))
+        time=$(( minutes / 60 )):$(printf %02d $(( minutes % 60 )))
+      fi
+      ;;
+    Charging)
+      symbol=$power
+      if [ "$(cat $BAT0/power_now)" = 0 ]
+      then
+        time='0:00'
+      else
+        minutes=$(( ($(cat $BAT0/energy_full) - $(cat $BAT0/energy_now)) * 60 / $(cat $BAT0/power_now) ))
+        time=$(( minutes / 60 )):$(printf %02d $(( minutes % 60 )))
+      fi
+      ;;
   esac
-fi
 
-if [ -f $BAT0/capacity ]
-then
   percent=$(cat $BAT0/capacity)
-fi
-
-if [ -f $BAT0/power_now ] && [ -f $BAT0/energy_now ]
-then
-  if [ "$(cat $BAT0/power_now)" = 0 ]
-  then
-    time='0:00'
-  else
-    minutes=$(( $(cat $BAT0/energy_now) * 60 / $(cat $BAT0/power_now) ))
-    time=$(( minutes / 60 )):$(printf %02d $(( minutes % 60 )))
-  fi
 fi
 
 if which pmset >/dev/null 2>/dev/null
 then
-  case $(pmset -g batt | egrep -o "AC|Battery" -m 1) in
+  case $(pmset -g batt | grep -E -o "AC|Battery" -m 1) in
     AC) symbol=$power ;;
     Battery) symbol=$battery ;;
   esac
   percent=$(pmset -g batt | ack -o "[[:digit:]]{1,3}(?=%)")
-  time=$(pmset -g batt | egrep -o "[[:digit:]]{1,2}:[[:digit:]]{2}")
+  time=$(pmset -g batt | grep -E -o "[[:digit:]]{1,2}:[[:digit:]]{2}")
 fi
 
-echo "$symbol $percent% $time"
+if [ "$percent" -lt 5 ]
+then
+  color="#[fg=#ffffd7,bg=#d70000]"
+fi
+
+echo "$color$symbol $percent% $time"
