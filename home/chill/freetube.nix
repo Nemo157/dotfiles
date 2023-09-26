2b953af2403884b4f9a5f6f62f1de45025da859a
Wim Looman <git@nemo157.com>
2023-09-26 16:04:32 +0200

    Use nightly build of freetube to avoid large history performance issues

--- home/chill/freetube.nix
+++ home/chill/freetube.nix
@@ -1,3 +1,11 @@
 { lib, config, pkgs-unstable, ... }: {
-  home.packages = [ pkgs-unstable.freetube ];
+  home.packages = [
+    (pkgs-unstable.freetube.overrideAttrs {
+      src = pkgs-unstable.requireFile {
+        name = "freetube_0.19.0-nightly-3500_amd64.AppImage.zip";
+        url = "https://github.com/FreeTubeApp/FreeTube/suites/16540961365/artifacts/944882863";
+        sha256 = "Omgn9Pbtm+AdpVZIcX4uKSCgVUHH+cmMuzOxZTrmT50=";
+      };
+    })
+  ];
 }
