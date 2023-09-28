d9f8794a6c4410315284f991210b1f492c547db7
Wim Looman <git@nemo157.com>
2023-09-28 09:30:23 +0200

    Add flatpak

--- home/mithril.nix
+++ home/mithril.nix
@@ -15,12 +15,16 @@
 
   home.packages = [
     pkgs.stc-cli
+    pkgs.flatpak
   ];
 
   xdg.userDirs = {
     enable = true;
     music = "${config.home.homeDirectory}/Music/Library";
   };
+  xdg.systemDirs.data = [
+    "${config.xdg.dataHome}/flatpak/exports/share"
+  ];
 
   services.syncthing = {
     enable = true;
