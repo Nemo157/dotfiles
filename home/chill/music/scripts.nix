82302873b80dc5d2ccacf7ac99ded8fde7af0ec9
Wim Looman <git@nemo157.com>
2023-09-28 22:28:12 +0200

    Make music widget toggle plaing, improve it

--- home/chill/music/scripts.nix
+++ home/chill/music/scripts.nix
@@ -12,30 +12,49 @@
   scripts.music-status = {
     runtimeInputs = [ pkgs.mpc-cli pkgs.jq pkgs.playerctl ];
     text = ''
-      if [ "$(mpc status '%state%')" == playing ]
-      then
-        playerctl -p mpd metadata artist | jq -R '{ artist: . }'
-        playerctl -p mpd metadata album | jq -R '{ album: . }'
-        playerctl -p mpd metadata mpris:artUrl | jq -R '{ albumart: sub("file://"; "") }'
-        playerctl -p mpd metadata title | jq -R '{ title: . }'
-        mpc status '%currenttime%\f%totaltime%\f%percenttime%\f%songpos%\f%length%' | jq -R '
+      {
+        playerctl -p mpd metadata mpris:artUrl | jq -R '{ albumart: sub("file://"; "") }' || true
+
+        mpc current -f '%artist%\f%album%\f%title%' | jq -R '
+          . / "\f" | {
+            current: {
+              artist: .[0],
+              album: .[1],
+              title: .[2],
+            }
+          }
+        ' || true
+
+        mpc status '%currenttime%\f%totaltime%' | jq -R '
           def lpad: tostring | ("0" * (2 - length)) + .;
           def parse_time: . / ":" | (60 * (.[0] | tonumber) + (.[1] | tonumber));
           def format_time: "\(. / 60 | floor):\(. % 60 | lpad)";
-          . / "\f" | {
-            time: {
-              current: .[0],
-              total: .[1],
-              remaining: ((.[1] | parse_time) - (.[0] | parse_time)) | format_time,
-              percent: (.[2] | gsub("[ %]"; "") | tonumber),
-            },
-            queue: {
-              position: .[3],
-              length: .[4],
+          . / "\f"
+            | (.[0] | parse_time) as $current
+            | (.[1] | parse_time) as $total
+            | {
+              time: {
+                current: $current | format_time,
+                total: $total | format_time,
+                remaining: ($total - $current) | format_time,
+                percent: ($current / if $total > 0 then $total else 1 end * 100),
+              },
             }
-          }
-        '
-      fi | jq -s 'map(to_entries) | flatten | from_entries'
+        ' || true
+
+        mpc status '%state%\f%songpos%\f%length%' | jq -R '
+          . / "\f"
+            | (.[1] | tonumber) as $position
+            | (.[2] | tonumber) as $length
+            | {
+              queue: {
+                state: (if $position == 0 then "stopped" else .[0] end),
+                position: $position,
+                length: $length,
+              }
+            }
+        ' || true
+      } | jq -s 'map(to_entries) | flatten | from_entries'
     '';
   };
 
--- home/desktop/eww.nix
+++ home/desktop/eww.nix
@@ -1,4 +1,12 @@
-{ lib, config, pkgs-unstable, ... }: {
+{ lib, config, pkgs, pkgs-unstable, ... }: {
+  xdg.dataFile."eww/no-album.png" = {
+    source = pkgs.requireFile {
+      name = "Generic-icon.png";
+      url = "https://icons.iconarchive.com/icons/musett/cds/256/Generic-icon.png";
+      sha256 = "CoOMVEYnZPTBAZcpD7N0XA5z4yNfOCe5EGBMF7zcViY=";
+    };
+  };
+
   programs.eww = {
     enable = true;
     package = pkgs-unstable.eww-wayland;
