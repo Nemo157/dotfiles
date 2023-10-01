db1a46da800094cfbfce8ad79419d7c9962cf554
Wim Looman <git@nemo157.com>
2023-10-01 11:34:34 +0200

    Install notofonts

--- home/desktop/fonts.nix
+++ home/desktop/fonts.nix
@@ -2,6 +2,10 @@
 {
   home.packages = with pkgs; [
     (nerdfonts.override { fonts = ["FiraCode"]; })
+    noto-fonts
+    noto-fonts-emoji
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
   ];
   fonts.fontconfig.enable = true;
 }
