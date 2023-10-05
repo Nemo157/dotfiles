0dcd0f80c4754f8735ec32cea5f970db1be41fbc
Wim Looman <git@nemo157.com>
2023-10-05 12:04:19 +0200

    Install dosbox

--- home/chill/default.nix
+++ home/chill/default.nix
@@ -1,8 +1,12 @@
-{ ... }: {
+{ pkgs, ... }: {
   imports = [
     ./freetube.nix
     ./sunshine.nix
     ./shairport-sync.nix
     ./music
+  ];
+
+  home.packages = with pkgs; [
+    dosbox-staging
   ];
 }
