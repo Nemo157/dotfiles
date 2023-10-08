353eff310a5ea98de42882373cb8f438b9ebcb79
Wim Looman <git@nemo157.com>
2023-10-08 23:33:17 +0200

    Extract ssh-keys, add mithrils

--- nixos/mithril/users.nix
+++ nixos/mithril/users.nix
@@ -3,10 +3,6 @@
     isNormalUser = true;
     extraGroups = [ "wheel" "video" "tty" "users" "input" "seat" ];
     shell = pkgs.zsh;
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM2xzZhKE1TzvJmhxT/q4v7vPC1zd9+OOjjaY/GUp73P"
-      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIP7NDZk2lJA+PejK0BiIgzRPX80THcMXzepuxHNiCNdlAAAABHNzaDo="
-      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIE5gbS2iAoFj4gbPQrvb/YIUN9WL91CKPPeT8brQ71omAAAABHNzaDo="
-    ];
+    openssh.authorizedKeys.keys = import ../ssh-keys.nix;
   };
 }
