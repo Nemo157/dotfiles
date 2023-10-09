1eb6613d06d8eb3536a6e720df1debab06da26c7
Wim Looman <git@nemo157.com>
2023-10-09 12:45:14 +0200

    Add broadcom firmware module

--- nixos/zinc/hardware/default.nix
+++ nixos/zinc/hardware/default.nix
@@ -1,4 +1,4 @@
-{
+{ pkgs, ... }: {
   imports = [
     ./drives.nix
     ./kernel.nix
@@ -13,4 +13,23 @@
       driSupport32Bit = true;
     };
   };
+
+  hardware.firmware = [
+    (pkgs.stdenvNoCC.mkDerivation rec {
+      pname = "apple-bcm-firmware";
+      version = "13.0";
+      rev = "1";
+
+      src = pkgs.fetchzip {
+        url = "https://github.com/NoaHimesaka1873/${pname}/releases/download/v${version}/${pname}-${version}-${rev}-any.pkg.tar.zst";
+        nativeBuildInputs = [ pkgs.zstd ];
+        stripRoot = false;
+        hash = "sha256-1uOefKdyBW4GJfhkJTjw3zDHDbHK77DrFScIekURpk8=";
+      };
+
+      installPhase = ''
+        cp -r usr/ "$out/"
+      '';
+    })
+  ];
 }
