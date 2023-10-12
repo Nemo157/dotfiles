e13020f164c00b8f52ca64f5e5850b5553c0342f
Wim Looman <git@nemo157.com>
2023-10-12 11:53:06 +0200

    Add zinc's key for listenbrainz token

--- secrets.nix
+++ secrets.nix
@@ -2,6 +2,6 @@ let
   mithril = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINaQ3qjviiCKZDmJvQ6L/LF9F/S1SRzIS009floRGzG0 nemo157@mithril";
   zinc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILml+0+R7rNJkgWBjALiO5gO63yQypkdSBWNMAT1SxQR nemo157@zinc";
 in {
-  "home/chill/music/listenbrainz-token.age".publicKeys = [ mithril ];
+  "home/chill/music/listenbrainz-token.age".publicKeys = [ mithril zinc ];
   "nixos/wlan-psk.age".publicKeys = [ zinc ];
 }
