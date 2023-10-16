b36bcbf5e73c212ae5979642b43692fdd34da3c1
Wim Looman <git@nemo157.com>
2023-10-16 14:39:06 +0200

    Add per-machine remote icons

--- home/desktop/alacritty/cross-machine.nix
+++ home/desktop/alacritty/cross-machine.nix
@@ -1,11 +1,23 @@
 { lib, config, pkgs, ... }: {
-  xdg.dataFile = lib.attrsets.mapAttrs' (host: name: {
+  xdg.dataFile = {
+    "icons/hicolor/256x256/apps/Mithril.png".source = pkgs.fetchurl {
+      url = "https://vignette.wikia.nocookie.net/lotrminecraftmod/images/4/41/OreMithril.png/revision/latest?cb=20160602204227";
+      name = "OreMithril.png";
+      hash = "sha256-gYqs+ILA+IAmnUny7OEphfweXVVBVDpS+Bp3rlDXF54=";
+    };
+
+    "icons/hicolor/512x512/apps/Zinc.png".source = pkgs.fetchurl {
+      url = "https://img3.wikia.nocookie.net/__cb20120824173419/metallurgy2/images/thumb/c/ca/ZincOre.png/500px-ZincOre.png";
+      hash = "sha256-Lf9z28rDfdyoliBcu77g07MntznHLZewz7y2QH8pVb4=";
+    };
+
+  } // lib.attrsets.mapAttrs' (host: name: {
     name = "applications/${host}.desktop";
     value.text = ''
       [Desktop Entry]
       Type=Application
       Name=${name}
-      Icon=Alacritty
+      Icon=${name}
       TryExec=alacritty
       Exec=alacritty --class ${name} --command ssh ${host} -t tmux -u new-session -s master-$(hostname -s) -t master -A
       StartupWMClass=${name}
