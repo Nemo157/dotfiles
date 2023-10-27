af6e5a814910b1b6647862153fc490223ae2486d
Wim Looman <git@nemo157.com>
2023-10-28 01:25:26 +0200

    Update rofi-wayland click-to-exit patch

--- overlays/rofi-wayland.nix
+++ overlays/rofi-wayland.nix
@@ -1,12 +1,12 @@
 { pkgs-final, pkgs-unstable, ... }:
 
 pkgs-unstable.rofi-wayland-unwrapped.overrideAttrs (final: prev: rec {
-  version = "f7fcb4c5a08e40846bfd5298ecc0de264e1eedb8";
+  version = "54464992eb11eda1f19829d04e492e3360b77d66";
   src = pkgs-final.fetchFromGitHub {
     owner = "Nemo157";
     repo = "rofi";
     rev = version;
     fetchSubmodules = true;
-    sha256 = "sha256-B7ljMbqV0Jw8nyChjlytoBCNdEqBSS2TudZv1yOa6/k=";
+    sha256 = "sha256-MllsDYN2eyi0h+Nsxi38E9fPb1bSJ4Y7MaTDbB1gF7Y=";
   };
 })
