84982a3e7b168e8be018fdfa11b452d1e3a47111
Wim Looman <git@nemo157.com>
2023-10-27 14:48:35 +0200

    Document firewall ports and restrict most interfaces

--- nixos/mithril/networking.nix
+++ nixos/mithril/networking.nix
@@ -3,10 +3,29 @@
     hostId = "69da08a2";
     hostName = "mithril";
     useDHCP = true;
-    firewall.allowedTCPPorts = [ 5000 47984 47989 48010 ];
-    firewall.allowedUDPPorts = [ 47998 47999 48000 48002 48010 ];
-    firewall.allowedUDPPortRanges = [
-      { from = 6001; to = 6011; }
-    ];
+    firewall = {
+      allowedTCPPorts = [
+        # shairport-sync
+        5000
+      ];
+
+      allowedUDPPortRanges = [
+        # shairport-sync
+        { from = 6001; to = 6011; }
+      ];
+
+      interfaces.tailscale0 = {
+        allowedTCPPorts = [
+          # sunshine
+          47984 47989 48010
+          # docs.rs
+          3000
+        ];
+        allowedUDPPorts = [
+          # sunshine
+          47998 47999 48000 48002 48010
+        ];
+      };
+    };
   };
 }
