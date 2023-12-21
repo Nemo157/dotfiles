8421860fc97a4d9871657432718ea3f48c8c2bd9
Wim Looman <git@nemo157.com>
2023-12-21 20:03:51 +0100

    Add shell to set required wasi envvars (for cargo at least)

--- shells/default.nix
+++ shells/default.nix
@@ -1,5 +1,6 @@
 { pkgs }:
 let
+  lib = pkgs.lib;
   mkRustShell = pkgs.callPackage ./rust { };
 in {
   rofi-wayland = pkgs.mkShell {
@@ -11,5 +12,13 @@ in {
 
   docs-rs = mkRustShell {
     extraBuildInputs = [ pkgs.sqlx-cli ];
+  };
+
+  wasi = pkgs.mkShell {
+    shellHook = ''
+      export CARGO_TARGET_WASM32_WASI_RUSTFLAGS="-Lnative=${pkgs.pkgsCross.wasi32.wasilibc}/lib"
+      export CARGO_TARGET_WASM32_WASI_RUSTDOCFLAGS="-Lnative=${pkgs.pkgsCross.wasi32.wasilibc}/lib"
+      export CARGO_TARGET_WASM32_WASI_RUNNER="${lib.getExe pkgs.wasmtime}"
+    '';
   };
 }
