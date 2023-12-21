63122d806cfac5b7100b60ae39131508f2e9dc07
Wim Looman <git@nemo157.com>
2023-12-21 20:01:28 +0100

    Add sunshine overlay
    disable unused systray, x11 and drm
    fix access to opengl libraries

---
+++ overlays/sunshine.nix
@@ -0,0 +1,22 @@
+{ pkgs-prev, pkgs-final, ... }:
+
+pkgs-prev.sunshine.overrideAttrs(final: prev: rec {
+  version = "336062d467d5f48ba56d05a523c212f791421108";
+  src = pkgs-final.fetchFromGitHub {
+    owner = "LizardByte";
+    repo = "Sunshine";
+    rev = version;
+    fetchSubmodules = true;
+    hash = "sha256-p2GZNL92QhQMprq/vpAwU0BW0eZGfRK2MwZ9LkBA0xM=";
+  };
+  separateDebugInfo = true;
+  runtimeDependencies = prev.runtimeDependencies ++ [
+    pkgs-final.libglvnd
+  ];
+  cmakeFlags = prev.cmakeFlags ++ [
+    "-DSUNSHINE_ENABLE_TRAY=OFF"
+    "-DSUNSHINE_REQUIRE_TRAY=OFF"
+    "-DSUNSHINE_ENABLE_X11=OFF"
+    "-DSUNSHINE_ENABLE_DRM=OFF"
+  ];
+})
