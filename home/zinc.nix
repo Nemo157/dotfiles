5742d5cc04ada58bd2e938bde2b701815a5639be
Wim Looman <git@nemo157.com>
2023-12-27 15:44:53 +0100

    Extract wluma to file

--- home/zinc.nix
+++ home/zinc.nix
@@ -5,6 +5,7 @@
     ./desktop
     ./xdg.nix
     ./age.nix
+    ./wluma.nix
   ];
 
   home.stateVersion = "23.05";
@@ -21,32 +22,4 @@
     tray.enable = true;
   };
 
-  xdg.configFile."wluma/config.toml".text = ''
-    [als.iio]
-    path = "/sys/bus/iio/devices"
-    thresholds = { 0 = "night", 2 = "dark", 8 = "dim", 15 = "normal", 50 = "bright", 100 = "outdoors" }
-
-    [[output.backlight]]
-    name = "integrated-screen"
-    path = "/sys/class/backlight/acpi_video0"
-    capturer = "wlroots"
-
-    [[keyboard]]
-    name = "integrated-keyboard"
-    path = "/sys/class/leds/apple::kbd_backlight"
-  '';
-
-  systemd.user.services.wluma = {
-    Unit = {
-      Description = pkgs.wluma.meta.description;
-      After = "graphical-session.target";
-    };
-    Service = {
-      Environment = "RUST_LOG=debug";
-      ExecStart = "${pkgs.wluma}/bin/wluma";
-    };
-    Install = {
-      WantedBy = [ "graphical-session.target" ];
-    };
-  };
 }
