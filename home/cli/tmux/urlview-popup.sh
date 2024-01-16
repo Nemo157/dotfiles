25e6b6f604febdf7b2d0127d7793d0c6d268bcd0
Wim Looman <git@nemo157.com>
2024-01-16 17:07:42 +0100

    Use alternate mode and support displaying a subset when the list is longer than the terminal height

--- home/cli/tmux/urlview-popup.sh
+++ home/cli/tmux/urlview-popup.sh
@@ -1,41 +1,65 @@
 trap exit SIGINT
-trap "echo -e '\e[?25h'" EXIT
+trap "echo -ne '\e[?1049l\e[?25h'" EXIT
 
 options=("$@")
 count=${#options[@]}
 selected=0
 
-echo -e '\e[?25l'
+echo -ne '\e[?25l\e[?1049h'
+
+read -r height < <(tput lines cols)
+# height=$(( height - 2 ))
 
 while true
 do
-    for i in "${!options[@]}"
-    do
-        if [ "$i" = "$selected" ]
-        then
-          echo -e "\e[40m  \e[95m»\e[39m  ${options[$i]}\e[K\e[0m"
-        else
-          echo -e "     ${options[$i]}\e[K"
-        fi
-    done
+  echo -ne "\e[H"
 
-    read -r -s -n1 key
+  offset=$(( selected - height / 2 ))
+  if (( offset < 0 ))
+  then
+    offset=0
+  elif (( offset > count - height ))
+  then
+    offset=$(( count - height ))
+  fi
 
-    case "$key" in
-      "k" )
-        selected=$(( selected == 0 ? selected : selected - 1 ))
-        ;;
-      "j" )
-        selected=$(( selected == count - 1 ? selected : selected + 1 ))
-        ;;
-      "" )
-        xdg-open "${options[$selected]}"
-        tmux display-message "Opened ${options[$selected]}"
-        ;&
-      "q" | $'\e' )
-        exit
-        ;;
-    esac
+  for (( i = 0 ; i < height && offset + i < count ; i++ ))
+  do
+    index=$(( offset + i ))
+    if (( index == selected ))
+    then
+      echo -ne "\e[40m  \e[95m»\e[39m"
+    elif (( i == 0 && index > 0 ))
+    then
+      echo -ne "  \e[95m▲\e[39m"
+    elif (( i == height - 1 && index < count - 1 ))
+    then
+      echo -ne "  \e[95m▼\e[39m"
+    else
+      echo -ne "   "
+    fi
+    echo -ne "  ${options[$index]}\e[K\e[0m"
+    if (( i != height - 1 ))
+    then
+      echo
+    fi
+  done
 
-    echo -ne "\e[${count}A"
+  read -r -s -n1 key
+
+  case "$key" in
+    "k" )
+      selected=$(( selected == 0 ? selected : selected - 1 ))
+      ;;
+    "j" )
+      selected=$(( selected == count - 1 ? selected : selected + 1 ))
+      ;;
+    "" )
+      xdg-open "${options[$selected]}"
+      tmux display-message "Opened ${options[$selected]}"
+      ;&
+    "q" | $'\e' )
+      exit
+      ;;
+  esac
 done
