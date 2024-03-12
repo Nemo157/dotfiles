f971cb973e9296d226b52f71744871a995f66333
Wim Looman <git@nemo157.com>
2024-03-12 10:56:53 +0100

    Use updated freetube with fixed yt api client

--- overlays/freetube.nix
+++ overlays/freetube.nix
@@ -1,6 +1,6 @@
-{ pkgs-final, pkgs-prev, ... }:
+{ pkgs-final, pkgs-unstable, ... }:
 pkgs-final.symlinkJoin {
-  inherit (pkgs-prev.freetube) name;
+  inherit (pkgs-unstable.freetube) name;
   paths = [
     (pkgs-final.writeShellApplication {
       name = "freetube";
@@ -8,11 +8,11 @@ pkgs-final.symlinkJoin {
       text = ''
         # https://github.com/NixOS/nixpkgs/issues/256401#issuecomment-1734912230
         # and remove some log spam
-        ${pkgs-prev.freetube}/bin/freetube "$@" ''${NIXOS_OZONE_WL:+''${WAYLAND_DISPLAY:+--enable-features=UseOzonePlatform --ozone-platform=wayland}} 2>&1 | sed \
-          -e '/Cannot create bo with format= R_8 and usage=SCANOUT_CPU_READ_WRITE/d' \
+        ${pkgs-unstable.freetube}/bin/freetube "$@" ''${NIXOS_OZONE_WL:+''${WAYLAND_DISPLAY:+--enable-features=UseOzonePlatform --ozone-platform=wayland}} 2>&1 | sed \
+          -e '/Cannot create bo with format/d' \
           -e '/GBM-DRV error (get_bytes_per_component): Unknown or not supported format: 538982482/d'
       '';
     })
-    pkgs-prev.freetube
+    pkgs-unstable.freetube
   ];
 }
