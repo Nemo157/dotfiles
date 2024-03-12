7d35775c9dc8e621f5897e447f263cfa69f15fab
Wim Looman <git@nemo157.com>
2024-03-12 10:55:08 +0100

    Add beets config to export music library to lossy for mobile devvices

--- home/chill/music/beets.nix
+++ home/chill/music/beets.nix
@@ -1,16 +1,31 @@
-{ lib, config, pkgs, ... }: {
+{ lib, config, pkgs, ... }:
+let
+  ffmpeg-fdk-aac = pkgs.ffmpeg.override {
+    withUnfree = true;
+    withFdkAac = true;
+  };
+in {
+  home.packages = [
+    pkgs.ffmpeg
+  ];
+
   programs.beets = {
     enable = true;
 
-    mpdIntegration.enableUpdate = true;
+    mpdIntegration = {
+      enableUpdate = true;
+      host = config.home.sessionVariables.MPD_HOST;
+    };
 
     settings = {
       directory = config.xdg.userDirs.music;
       library = "${config.xdg.userDirs.music}/.beets.db";
 
       plugins = [
+        "convert"
         "embedart"
         "fetchart"
+        "fromfilename"
         "info"
         "mbsync"
         "missing"
@@ -32,6 +47,22 @@
 
       fetchart = {
         sources = [ "filesystem" "coverart" ];
+      };
+
+      convert = {
+        # can't use `auto = true` as that ignores the `dest` and instead
+        # replaces the files that get put into the library, will have to
+        # `beets convert` manually after importing new albums to generate
+        copy_album_art = true;
+        album_art_maxwidth = 1024;
+        dest = "${config.home.homeDirectory}/Music/Low";
+        embed = false;
+        max_bitrate = 128;
+        formats.aac = {
+          command = "${lib.getExe ffmpeg-fdk-aac} -i $source -y -vn -c:a libfdk_aac -vbr 4 $dest";
+          extension = "m4a";
+        };
+        format = "aac";
       };
     };
   };
