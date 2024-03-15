23f95a513d3ad50c6a73f654e1ce8d34293db069
Wim Looman <git@nemo157.com>
2024-03-15 15:20:04 +0100

    Add a bunch more telegraf metrics

--- nixos/mithril/services/telegraf.nix
+++ nixos/mithril/services/telegraf.nix
@@ -1,9 +1,27 @@
-{ ts, ... }: {
+{ lib, pkgs, ts, config, ... }: {
   services.telegraf = {
     enable = true;
     extraConfig = {
       inputs = {
         cpu = {};
+        docker = {};
+        linux_cpu = {};
+        mem = {};
+        net = {
+          ignore_protocol_stats = true;
+        };
+        netstat = {};
+        nvidia_smi = {
+          bin_path = lib.getExe' config.hardware.nvidia.package "nvidia-smi";
+        };
+        processes = {};
+        sensors = {};
+        swap = {};
+        temp = {};
+        zfs = {
+          poolMetrics = true;
+          datasetMetrics = true;
+        };
       };
       outputs = {
         influxdb = {
@@ -12,4 +30,8 @@
       };
     };
   };
+
+  users.users.telegraf.extraGroups = [ "docker" ];
+
+  systemd.services.telegraf.path = [ pkgs.lm_sensors ];
 }
