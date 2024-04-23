0177fc040df1b17564f5874e5454bf423d989711
Wim Looman <git@nemo157.com>
2024-04-23 17:07:27 +0200

    Grab metrics for scoped app slices, and for the top-level slices

--- nixos/common/services/telegraf.nix
+++ nixos/common/services/telegraf.nix
@@ -10,9 +10,14 @@
             "/sys/fs/cgroup/system.slice/*.service"
             "/sys/fs/cgroup/user.slice"
             "/sys/fs/cgroup/user.slice/user-1000.slice"
+            "/sys/fs/cgroup/user.slice/user-1000.slice/user@1000.service/app.slice"
             "/sys/fs/cgroup/user.slice/user-1000.slice/user@1000.service/app.slice/app-*.scope"
+            "/sys/fs/cgroup/user.slice/user-1000.slice/user@1000.service/app-*.slice"
+            "/sys/fs/cgroup/user.slice/user-1000.slice/user@1000.service/app-*.slice/app-*.scope"
             "/sys/fs/cgroup/user.slice/user-1000.slice/user@1000.service/app.slice/*.service"
+            "/sys/fs/cgroup/user.slice/user-1000.slice/user@1000.service/background.slice"
             "/sys/fs/cgroup/user.slice/user-1000.slice/user@1000.service/background.slice/*.service"
+            "/sys/fs/cgroup/user.slice/user-1000.slice/user@1000.service/session.slice"
             "/sys/fs/cgroup/user.slice/user-1000.slice/user@1000.service/session.slice/*.service"
           ];
           files = [
