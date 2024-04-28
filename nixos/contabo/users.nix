ee332ea4d965752e863b073d503dc70313206975
Wim Looman <git@nemo157.com>
2024-04-28 14:49:59 +0200

    Add password for contabo user account

--- nixos/contabo/users.nix
+++ nixos/contabo/users.nix
@@ -7,6 +7,7 @@
       home = "/home/nemo157";
       extraGroups = ["wheel"];
       openssh.authorizedKeys.keys = import ../ssh-keys.nix;
+      hashedPassword = "$y$j9T$E6IJdj3Qsa1ZUpR4fTNP..$WE5sYB3PmotlsD.IRz6kNuIyWYG4VOLa3yosbVnPNk/";
     };
   };
 }
