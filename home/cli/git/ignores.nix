4cbe96eff0c5a5886cc261cbd2bc4836494489a7
Wim Looman <git@nemo157.com>
2024-05-21 12:53:41 +0200

    Ignore bacon.toml since it's mostly an empty file

--- home/cli/git/ignores.nix
+++ home/cli/git/ignores.nix
@@ -16,5 +16,8 @@
     "compile_commands.json"
     ".bacon-locations"
     ".direnv/"
+    # Because of https://github.com/Canop/bacon/issues/157 need to create an
+    # empty bacon.toml in all repos to use it...
+    "bacon.toml"
   ];
 }
