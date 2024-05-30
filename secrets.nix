51a9a18ac643c421a5bc78050c074c6c5bdb4b6b
Wim Looman <wim.looman@veecle.io>
2024-05-30 15:15:12 +0200

    Add oak pubkeys for secrets

--- secrets.nix
+++ secrets.nix
@@ -1,10 +1,12 @@
 let
   nemo157-mithril = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINaQ3qjviiCKZDmJvQ6L/LF9F/S1SRzIS009floRGzG0 nemo157@mithril";
   nemo157-zinc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILml+0+R7rNJkgWBjALiO5gO63yQypkdSBWNMAT1SxQR nemo157@zinc";
+  wim-oak = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPeQegyhCB7UkgG4LwcyHbOs9zmBsonfnPQg1HkKZes2 wim@oak";
 
   mithril = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINuIJQEiQuK0HxfPjk28NUkqJtzNQpv0nwJ55rQK4PKd";
   zinc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMAmICnjg5MYSLmEJ5YWSilEzWwtjAh6+iOFZ/vAAGCo";
   contabo = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBap8/dDxDUVLcVn4U/bIGT44kw4Amm54kaRUwxdYEdZ";
+  oak = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ0KsxqQllBb1Hy76Lrj52uZaLjqRNRyt60Qdecw8sER";
 in {
   "home/chill/music/listenbrainz-token.age".publicKeys = [ nemo157-mithril nemo157-zinc ];
   "home/veecle/ssh-config.age".publicKeys = [ nemo157-zinc ];
