bcc6f5c3a58f7ebf51594954f996f81a594a359c
Wim Looman <wim.looman@veecle.io>
2024-05-30 15:17:56 +0200

    Add wifi keys

--- nixos/oak/networking.nix
+++ nixos/oak/networking.nix
@@ -1,9 +1,16 @@
-{
+{ config, ... }: {
+  age.secrets.wlan-psk.file = ../wlan-psk.age;
+
   networking = {
     hostName = "oak";
     wireless = {
       enable = true;
       userControlled.enable = true;
+      environmentFile = config.age.secrets.wlan-psk.path;
+      networks = {
+        NineEyes.psk = "@PSK_NINE_EYES@";
+        Drivery.psk = "@PSK_DRIVERY@";
+      };
     };
   };
 }
