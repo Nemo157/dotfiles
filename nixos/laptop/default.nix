4a3a1be38afa3d3f5fdaee426ac29141839eb3dd
Wim Looman <wim.looman@veecle.io>
2024-06-23 10:52:13 +1200

    Add static geoclue location since MLS has shut down

--- nixos/laptop/default.nix
+++ nixos/laptop/default.nix
@@ -10,10 +10,28 @@
 
     geoclue2 = {
       enable = true;
+
+      # https://github.com/NixOS/nixpkgs/issues/321121
+      enableWifi = false;
+
       appConfig.darkman = {
         isAllowed = true;
         isSystem = false;
       };
     };
+  };
+
+  # workaround above issue by manually specifying location
+  environment.etc = {
+    "geolocation".text = ''
+      41.4
+      173.0
+      0
+      160000
+    '';
+    "geoclue/conf.d/50-static-source.conf".text = ''
+      [static-source]
+      enable = true
+    '';
   };
 }
