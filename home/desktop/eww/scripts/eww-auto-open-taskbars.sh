482751c35481dac18ab8aace13f334d7b986b1dd
Wim Looman <wim.looman@veecle.io>
2024-07-15 09:06:19 +0200

    Retry eww-auto-open-taskbars if it fails (as can happen after waking from sleep)

--- home/desktop/eww/scripts/eww-auto-open-taskbars.sh
+++ home/desktop/eww/scripts/eww-auto-open-taskbars.sh
@@ -10,14 +10,17 @@ open-taskbars() {
 
 open-taskbars
 
-SOCKET="$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock"
-socat -u "UNIX-CONNECT:$SOCKET,forever" - | while read -r event
+while true
 do
-  if [ "${event%>>*}" == "monitoradded" ]
-  then
-    echo "monitor added event detected" >&2
-    eww close-all
-    open-taskbars
-    systemctl --user start --no-block swww-change-wallpaper
-  fi
+  SOCKET="$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock"
+  socat -u "UNIX-CONNECT:$SOCKET,forever" - | while read -r event
+  do
+    if [ "${event%>>*}" == "monitoradded" ]
+    then
+      echo "monitor added event detected" >&2
+      eww close-all
+      open-taskbars
+      systemctl --user start --no-block swww-change-wallpaper
+    fi
+  done
 done
