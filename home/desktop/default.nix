f31bd00d6f53e02ea038a719a50b86f118b32b03
Wim Looman <git@nemo157.com>
2024-07-19 15:06:28 +0200

    Update u2f-touch-detector and add notification config

--- home/desktop/default.nix
+++ home/desktop/default.nix
@@ -26,7 +26,21 @@
       options = [ "caps:escape" "compose:ralt" ];
   };
 
-  services.u2f-touch-detector.enable = true;
+  services.u2f-touch-detector = {
+    enable = true;
+    settings = {
+      notify = {
+        enable = true;
+        heading = "I can haz touch?";
+        message = "";
+        image = pkgs.requireFile {
+          name = "ferrisPlead.png";
+          url = "https://media.discordapp.net/attachments/536629832080162846/857441451444666408/ferrisPlead.png?ex=669b1b94&is=6699ca14&hm=4018ba4f751cf2bc228e17b052af24004bb1e52eb7694a13e0200f0f24c9e75e&format=png";
+          sha256 = "1rx5xddh9dz5jp4pnnk6yi20ydyv2k6m5463zh8mx66qznbhrx7k";
+        };
+      };
+    };
+  };
 
   xdg.mimeApps = {
     enable = true;
