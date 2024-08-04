361682d43f4461e01ededa941bdf16d4ffb63d32
Wim Looman <git@nemo157.com>
2024-08-04 11:24:50 +0200

    Try and have dynamic cursor color (though doesn't really seem to work)

--- home/desktop/cursor.nix
+++ home/desktop/cursor.nix
@@ -1,16 +1,29 @@
 { config, pkgs, ... }: {
   home.pointerCursor = {
-    package = pkgs.catppuccin-cursors.latteRosewater;
-    name = "catppuccin-latte-rosewater-cursors";
+    package = pkgs.catppuccin-cursors.latteLight;
+    name = "catppuccin-latte-light-cursors";
     gtk.enable = true;
     x11.enable = true;
   };
 
-  # For flatpak apps to access cursors they need to be in ~/.local/share
-  # Unfortunately this just creates symlinks the flatpaks can't access, you
-  # still need an override added to access the nix store:
-  #     flatpak override -u --filesystem /nix/store:ro
-  xdg.dataFile = with config.home.pointerCursor; {
-    "icons/${name}".source = "${package}/share/icons/${name}";
+  xdg.dataFile = {
+    # For flatpak apps to access cursors they need to be in ~/.local/share
+    # Unfortunately this just creates symlinks the flatpaks can't access, you
+    # still need an override added to access the nix store:
+    #     flatpak override -u --filesystem /nix/store:ro
+    "icons/catppuccin-latte-light-cursors".source = "${pkgs.catppuccin-cursors.latteLight}/share/icons/catppuccin-latte-light-cursors";
+    "icons/catppuccin-mocha-dark-cursors".source = "${pkgs.catppuccin-cursors.mochaDark}/share/icons/catppuccin-mocha-dark-cursors";
+
+    "light-mode.d/hyprcursor-light.sh" = {
+      source = pkgs.writeShellScript "hyprcursor-light.sh" ''
+        ${pkgs.hyprland}/bin/hyprctl setcursor catppuccin-mocha-dark-cursors 32
+      '';
+    };
+
+    "dark-mode.d/hyprcursor-dark.sh" = {
+      source = pkgs.writeShellScript "hyprcursor-dark.sh" ''
+        ${pkgs.hyprland}/bin/hyprctl setcursor catppuccin-latte-light-cursors 32
+      '';
+    };
   };
 }
