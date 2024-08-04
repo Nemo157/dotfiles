03aa607b0e0f2741c8592ce4b17af7218c1986e8
Wim Looman <git@nemo157.com>
2024-08-04 09:47:20 +0200

    Use local .editorconfig as default global too

--- home/dev/editorconfig.nix
+++ home/dev/editorconfig.nix
@@ -1,34 +1,5 @@
 {
   home.file = {
-    ".editorconfig".text = ''
-      root = true
-
-      [*]
-      end_of_line = lf
-      charset = utf-8
-      trim_trailing_whitespace = true
-      insert_final_newline = true
-      indent_style = space
-      indent_size = 4
-      max_line_length = 80
-
-      [*.rs]
-      max_line_length = 100
-
-      [*.nix]
-      max_line_length = 100
-
-      [*.md]
-      max_line_length = 0
-
-      [*.{ts,js}]
-      indent_size = 2
-
-      [*.toml]
-      indent_size = 2
-
-      [*.{bash,sh,zsh}]
-      indent_size = 2
-    '';
+    ".editorconfig".source = ../../.editorconfig;
   };
 }
