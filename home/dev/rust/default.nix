2998ed5f3af76e82471e4e2768a2de9f7445c43e
Wim Looman <git@nemo157.com>
2024-12-04 15:23:55 +0100

    Disable for-each-rust because it takes way too long to build all the time and doesn't cache properly

--- home/dev/rust/default.nix
+++ home/dev/rust/default.nix
@@ -4,16 +4,17 @@
     ./cargo.nix
   ];
 
-  scripts.for-each-rust = {
-    text = let
-      rusts = lib.filterAttrs (v: c: (lib.hasPrefix "1." v)) pkgs.rust-bin.stable;
-      scripts = lib.mapAttrsToList (v: c: ''
-        printf "\e[1m==== %s ====\e[0m\n" "${v}"
-        echo
-        PATH="${if c ? "minimal" then c.minimal else c.rust}/bin:$PATH" "$@" || true
-        echo
-        echo
-      '') rusts;
-    in builtins.concatStringsSep "\n" scripts;
-  };
+
+  # scripts.for-each-rust = {
+  #   text = let
+  #     rusts = lib.filterAttrs (v: c: (lib.hasPrefix "1." v)) pkgs.rust-bin.stable;
+  #     scripts = lib.mapAttrsToList (v: c: ''
+  #       printf "\e[1m==== %s ====\e[0m\n" "${v}"
+  #       echo
+  #       PATH="${if c ? "minimal" then c.minimal else c.rust}/bin:$PATH" "$@" || true
+  #       echo
+  #       echo
+  #     '') rusts;
+  #   in builtins.concatStringsSep "\n" scripts;
+  # };
 }
