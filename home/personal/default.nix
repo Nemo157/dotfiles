4a79a321c7097a4f68f4b3aad5c560aba3a99030
Wim Looman <git@nemo157.com>
2025-03-15 14:42:01 +0100

    install calibre

--- home/personal/default.nix
+++ home/personal/default.nix
@@ -7,6 +7,10 @@
     ./ssh.nix
   ];
 
+  home.packages = with pkgs; [
+    calibre
+  ];
+
   # Tray isn't available until eww starts up
   systemd.user.services.syncthingtray.Service.ExecStart = lib.mkIf config.services.syncthing.enable
     (lib.mkForce "${lib.getExe' pkgs.syncthingtray-minimal "syncthingtray"} --wait");
