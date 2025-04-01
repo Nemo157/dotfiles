a2d56d9964edded58c911a022b4e82f6189d4724
Wim Looman <git@nemo157.com>
2025-04-01 11:17:12 +0200

    fix swayimg args (and now it needs a pause before starting slurp)

--- home/desktop/hypr/land.nix
+++ home/desktop/hypr/land.nix
@@ -7,9 +7,12 @@ in {
     text = ''
       # ideas stolen from https://github.com/emersion/slurp/issues/104#issuecomment-1381110649
 
-      grim - | swayimg --background none --scale real --no-sway --fullscreen - &
+      grim - | swayimg --scale real --fullscreen - &
       # shellcheck disable=SC2064
       trap "kill $!" EXIT
+
+      sleep 1
+
       grim -t png -g "$(slurp)" - | wl-copy -t image/png
     '';
   };
