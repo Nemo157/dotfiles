83b44d7f4ae41414774e9493360de33dbc1c7637
Wim Looman <git@nemo157.com>
2026-01-13 13:02:38 +0100

    configure oak networking directly

--- nixos/oak/networking.nix
+++ nixos/oak/networking.nix
@@ -7,4 +7,21 @@
   services.tailscale = {
     enable = true;
   };
+
+  systemd.network.networks = {
+    "50-wlan" = {
+      matchConfig = {
+        Type = "wlan";
+      };
+      networkConfig = {
+        DHCP = "ipv4";
+        IPv6AcceptRA = true;
+        IPv6LinkLocalAddressGenerationMode = "random";
+        IPv6PrivacyExtensions = true;
+      };
+      linkConfig = {
+        RequiredForOnline = "yes";
+      };
+    };
+  };
 }
