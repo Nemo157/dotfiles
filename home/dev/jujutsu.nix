cdefbc89d9ec61a2ba3d83024ea730525a8c9680
Wim Looman <git@nemo157.com>
2026-03-19 11:00:54 +0100

    get rid of silly main alias

--- home/dev/jujutsu.nix
+++ home/dev/jujutsu.nix
@@ -112,7 +112,6 @@
         "pulls" = "remote_bookmarks(pulls/)";
         "interesting_heads" = "visible_heads() ~ remote_bookmarks() | bookmarks()";
         "trunk" = "trunk()";
-        "main" = "trunk()";
         "all" = "all()";
       };
 
