82ac9328c9b132cb60ce8e2eaeb033b6a4223ae1
Wim Looman <git@nemo157.com>
2026-05-14 20:42:48 +0200

    add direnv note to claude memory on `lime`

--- home/lime.nix
+++ home/lime.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ lib, ... }: {
   imports = [
     ./cli
     ./common.nix
@@ -14,5 +14,13 @@
 
   home.stateVersion = "25.11";
 
-  programs.claude-code.configDirOverride = false;
+  programs.claude-code = {
+    configDirOverride = false;
+
+    memory = lib.mkAfter ''
+
+      Most projects will have a `.envrc` providing project-specific tools, you need to use `direnv exec` to run these.
+      General system or broadly used development tools do not need this and should be available in the environment directly.
+    '';
+  };
 }
